deeb64e1ac3a3b71d535a2ea7c7765d61552d82a
zgeorgiev <zdravko.g@me.com>
2020-01-19 01:58:59 +0200

    Moved JS to separate file

---
+++ Hubs/Chat.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace RoomsChat.Hubs
+{
+    public class Chat : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
